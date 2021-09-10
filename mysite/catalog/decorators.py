4dc7e018e5792f83204393f327531815a86e2df4
Listonoh <leestackcz@gmail.com>
2021-09-10 16:58:16 +0200

    exceptions and strange pages

---
+++ mysite/catalog/decorators.py
@@ -0,0 +1,25 @@
+import functools
+
+from django.shortcuts import render
+from requests.exceptions import ConnectionError, RequestException
+
+
+BASE_ERROR_MESSAGE = 'Could not connect to your box because {error_reason}'
+
+
+def handle_view_exception(func):
+    """Decorator for handling exceptions."""
+    @functools.wraps(func)
+    def wrapper(request, *args, **kwargs):
+        try:
+            response = func(request, *args, **kwargs)
+        except RequestException as e:
+            error_reason = 'of an unknown error.'
+            if isinstance(e, ConnectionError):
+                error_reason = 'the host is unknown.'
+            context = {
+                'error_message': BASE_ERROR_MESSAGE.format(error_reason=error_reason),
+            }
+            response = render(request, 'error.html', context)
+        return response
+    return wrapper
--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,7 +1,8 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
-from numpy import result_type
 from django.core.paginator import Paginator
 import requests
+from .decorators import handle_view_exception
 
 # Create your views here.
 
@@ -10,20 +11,29 @@ def get_page_obj_from_result(request_result, page_number):
     current_page = int(request_result.headers["x-pagination-current-page"])
     page_count = int(request_result.headers["x-pagination-page-count"])
     per_page = int(request_result.headers["x-pagination-per-page"])
-    # print(current_page, page_count, per_page)
 
     extended_result_for_paginator = [None] * (current_page-1)*per_page + \
         request_result.json() + [None] * (page_count - current_page)*per_page
-    # print(extended_result)
     p = Paginator(extended_result_for_paginator, per_page)
     return p.get_page(page_number)
 
 
+def clean_int_input(number: str, default_result=1) -> int:
+    if not number.isnumeric():
+        return default_result
+    return int(number)
+
+
+@handle_view_exception
 def index(request):
-    page_number = int(request.GET.get('page', 1))
-    per_page = int(request.GET.get('per-page', 5))
+    page_number = clean_int_input(request.GET.get('page', '1'))
+    per_page = clean_int_input(request.GET.get('per-page', '5'), 5)
     target_url = f'https://faktury.mzcr.cz/rest/organizations/?page={page_number}&per-page={per_page}'
     result = requests.get(target_url)
+    result.raise_for_status()
+
+    if per_page < 1 or int(result.headers["x-pagination-page-count"]) < page_number or page_number < 1:
+        raise Http404()
     organizations = get_page_obj_from_result(result, page_number)
 
     context = {"organizations": organizations, "per_page": per_page}
